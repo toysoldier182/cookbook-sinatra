092c0a937abb721bdba5e8f27d20b497a1a3f43b
juancarlosasensio <jasensio@ufm.edu>
2020-01-27 18:14:51 +0000

    Using sinatra

--- app.rb
+++ app.rb
@@ -7,6 +7,15 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
-get '/' do
-  'This is me spinning up a local web browser.'
+get '/' do # Router
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/team/:username' do
+  puts params[:username]
+  "The username is #{params[:username]}"
 end
